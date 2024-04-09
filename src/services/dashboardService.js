c29592f020d7df333466933e5f35b8b3810fded7
Rogelio Olarte <galaxino12@gmail.com>
2024-04-09 13:14:02 -0500

    modified styles and connected service

---
+++ src/services/dashboardService.js
@@ -0,0 +1,12 @@
+export const obtainProduct = async () => {
+    return await fetch('')
+        .then(() => {return true})
+        .catch()
+        .finally()
+}
+export const sendOffer = async () => {
+    return await fetch('')
+        .then(() => {return true})
+        .catch()
+        .finally()
+}
