df52221addc69679c344e0cc9e8b859ad4734720
Rogelio Olarte <galaxino12@gmail.com>
2024-04-09 16:32:18 -0500

    minor change on dashboardService

--- src/services/dashboardService.js
+++ src/services/dashboardService.js
@@ -2,16 +2,21 @@
 /* import { MAIN_API, ROUTE_GET_PRODUCTS, ROUTE_SEND_OFFER } from '../config/api_routes' */
 
 export const obtainProduct = async () => {
-    let response = await fetch('https://reqres.in/api/users?page=1')
+    return await fetch(`http://localhost:8080/api/currency/all`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            /* 'token': JSON.stringify(TOKEN) */
+        },
+    })
         .then(response => {
             if (!response.ok) throw new Error('Failed to fetch users');
-            return response.json()
+            console.log('Response from service:', response);
+            return JSON.stringify(response.body)
         })
         .then(response => console.log(response.data))
         .catch((error) => console.error(`Error: ${error}`))
         .finally(()=>console.info('Request of products finished'))
-    console.log('Response:', response);
-    return []
 }
 export const sendOffer = async (data) => {
     return await fetch('https://reqres.in/api/users', {
