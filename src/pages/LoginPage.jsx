634f0b39e2f737607608074c1203d0d3521c09fc
Rogelio Olarte <galaxino12@gmail.com>
2024-04-08 22:03:17 -0500

    update files

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,9 @@
-import LoginFormik from "../components/pure/forms/LoginFormik";
-
+import LoginFormik from '../components/pure/forms/LoginFormik'
 
 function LoginPage() {
-
   return (
-    <div >
-      <LoginFormik />
-    </div>
-  );
+    <div><LoginFormik/></div>
+  )
 }
 
-export default LoginPage;
+export default LoginPage
