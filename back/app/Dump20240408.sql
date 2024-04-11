1dea2bcba41e8a984b643c2bc90073bfc2dc58ad
lionel staricoff <staricofflionel@hotmail.com>
2024-04-11 10:19:03 -0500

    creando base de datos nueva

--- back/app/Dump20240408.sql
+++ back/app/Dump20240408.sql
@@ -80,8 +80,8 @@ DROP TABLE IF EXISTS `transactions`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `transactions` (
   `transaction_id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `product_id` int NOT NULL,
+  `user_id` int,
+  `product_id` int,
   `transaction_type` enum('buy','sell') NOT NULL,
   `quantity` int NOT NULL,
   `price_per_unit` decimal(18,2) NOT NULL,
