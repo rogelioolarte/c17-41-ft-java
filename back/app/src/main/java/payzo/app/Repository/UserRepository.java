5a337c897587caadad92fe40cf88a54adfb5f291
lionel staricoff <staricofflionel@hotmail.com>
2024-04-13 12:02:57 -0500

    restrurando el objeto del login pero lo sigue trayendo null

--- back/app/src/main/java/payzo/app/Repository/UserRepository.java
+++ back/app/src/main/java/payzo/app/Repository/UserRepository.java
@@ -16,9 +16,9 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT u.userId,u.name,u.avatar,u.wallet FROM User u WHERE u.email =:email AND u.password =:password")
-    User findByEmailAndPassword(@Param("email")String email,@Param("password") String password);
-
+  /*  @Query("SELECT u FROM User u WHERE u.email =:email AND u.password =:password")
+    User findByEmailAndPassword(@Param("email")String email,@Param("password") String password);*/
+    User findByEmailAndPassword(String email,String password);
 
     @Query("SELECT NEW payzo.app.dto.CurrencyDtoList(t.currency.cryptoId, t.currency.productName, " +
             "SUM(CASE WHEN t.type = 'buy' THEN t.quantity ELSE -t.quantity END)) " +
