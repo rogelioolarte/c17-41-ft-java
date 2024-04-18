c3607f8c57484ea0e855f0608b0b8ec652484da5
Rogelio Olarte <galaxino12@gmail.com>
2024-04-17 22:05:47 -0500

    added the avatar field and commented for dev

--- front/src/models/user.schema.js
+++ front/src/models/user.schema.js
@@ -1,13 +1,13 @@
-import * as Yup from "yup";
+import * as Yup from 'yup';
 
 const userSchema = Yup.object().shape({
-  firstName: Yup.string().required("Please enter your first name"),
-  lastName: Yup.string().required("Please enter your last name"),
+  firstName: Yup.string().required('Please enter your first name'),
+  lastName: Yup.string().required('Please enter your last name'),
   idPassport: Yup.string()
-    .required("Please give us an ID or Passport number")
+    .required('Please give us an ID or Passport number')
     .test(
-      "valid format",
-      "Format must be that of either an ID or a Passport",
+      'valid format',
+      'Format must be that of either an ID or a Passport',
       (value) => {
         const dniRegex = /^\d{7,8}$/;
         const passportRegex = /^P\d{7}$/;
@@ -15,20 +15,30 @@ const userSchema = Yup.object().shape({
       }
     ),
   email: Yup.string()
-    .email("Must be a valid email format")
-    .required("Please enter your email"),
+    .email('Must be a valid email format')
+    .required('Please enter your email'),
   password: Yup.string()
-    .min(8, "Password too short")
+    .min(8, 'Password too short')
     .test(
-      "has-uppercase",
-      "Password must contain at least one uppercase letter",
+      'has-uppercase',
+      'Password must contain at least one uppercase letter',
       (value) => {
         return /[A-Z]/.test(value);
       }
     )
-    .required("Please enter a password"),
-  avatar: Yup.string(),
-  account: Yup.string().required("Please enter your account number"),
+    .required('Please enter a password'),
+  /* avatar: Yup.mixed()
+    .test('fileSize', 'File size too large', (value) => {
+      if (!value) return true;
+      return value.size <= 1024 * 1024; // Máximo 1MB
+    })
+    .test('fileFormat', 'Invalid file format', (value) => {
+      if (!value) return true;
+      return (
+        value.type === 'image/png' || value.type === 'image/jpeg'
+      ); 
+    }), */
+  account: Yup.string().required('Please enter your account number'),
 });
 
 export default userSchema;
