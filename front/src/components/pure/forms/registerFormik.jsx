a9e264404bf05f07aed3cb3332b40e38aea422fb
ronanfilloy <ronanfilloy@gmail.com>
2024-04-18 15:26:51 -0300

    replacing local code

--- front/src/components/pure/forms/registerFormik.jsx
+++ front/src/components/pure/forms/registerFormik.jsx
@@ -1,4 +1,4 @@
-import { useContext, /* useState */ } from "react";
+import { useContext /* useState */ } from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { UserContext } from "../../../contexts/user.context";
@@ -12,8 +12,15 @@ const RegisterFormik = () => {
   const navigate = useNavigate();
   /* const [avatarPreview, setAvatarPreview] = useState(null); */
 
-  const initialValues = { firstName: "", lastName: "", idPassport: "",
-    email: "", password: "", avatar: "", account: "", };
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    idPassport: "",
+    email: "",
+    password: "",
+    avatar: "",
+    account: "",
+  };
 
   const navigateToErrorPage = (error) => {
     navigate(`/error?message=${encodeURIComponent(error)}`);
@@ -22,12 +29,19 @@ const RegisterFormik = () => {
   const handleSubmit = async (values) => {
     // Send avatar as base64
     /* values.avatar = avatarPreview; */
-    const newUser = await register( values.firstName, values.lastName, values.idPassport,
-      values.email, values.password, /* values.avatar */ "", values.account, navigateToErrorPage
+    const newUser = await register(
+      values.firstName,
+      values.lastName,
+      values.idPassport,
+      values.email,
+      values.password,
+      /* values.avatar */ "",
+      values.account,
+      navigateToErrorPage
     );
     if (newUser.data) {
       assignUserInfo(newUser.data);
-      navigate('/dashboard');
+      navigate("/dashboard");
     }
   };
 
@@ -54,39 +68,89 @@ const RegisterFormik = () => {
           <img src={Logo} />
         </Link>
       </nav>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={userSchema} >
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={userSchema}
+      >
         <Form className="register-form">
           <h1 className="login-register-title">Create an account</h1>
 
           <div className="login-register-input">
             <label htmlFor="first-name">First Name</label>
-            <Field type="text" id="first-name" name="firstName" className="register-field" autoFocus />
-            <ErrorMessage name="firstName" component="div" className="error-message" />
+            <Field
+              type="text"
+              id="first-name"
+              name="firstName"
+              className="register-field"
+              autoFocus
+            />
+            <ErrorMessage
+              name="firstName"
+              component="div"
+              className="error-message"
+            />
           </div>
 
           <div className="login-register-input">
             <label htmlFor="last-name">Last Name</label>
-            <Field type="text" id="last-name" name="lastName" className="register-field" />
-            <ErrorMessage name="lastName" component="div" className="error-message" />
+            <Field
+              type="text"
+              id="last-name"
+              name="lastName"
+              className="register-field"
+            />
+            <ErrorMessage
+              name="lastName"
+              component="div"
+              className="error-message"
+            />
           </div>
 
           <div className="login-register-input">
             <label htmlFor="id-passport">ID or Passport No.</label>
-            <Field type="text" id="id-passport" name="idPassport" className="register-field" />
-            <ErrorMessage name="idPassport" component="div" className="error-message" />
+            <Field
+              type="text"
+              id="id-passport"
+              name="idPassport"
+              className="register-field"
+            />
+            <ErrorMessage
+              name="idPassport"
+              component="div"
+              className="error-message"
+            />
           </div>
 
           <div className="login-register-input">
             <label htmlFor="email">Email</label>
-            <Field type="email" id="email" name="email" 
-              placeholder="Example@email.com" className="register-field" />
-            <ErrorMessage name="email" component="div" className="error-message" />
+            <Field
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Example@email.com"
+              className="register-field"
+            />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className="error-message"
+            />
           </div>
 
           <div className="login-register-input">
             <label htmlFor="password">Password</label>
-            <Field type="password" id="password" name="password" className="register-field" />
-            <ErrorMessage name="password" component="div" className="error-message" />
+            <Field
+              type="password"
+              id="password"
+              name="password"
+              className="register-field"
+            />
+            <ErrorMessage
+              name="password"
+              component="div"
+              className="error-message"
+            />
           </div>
 
           {/* <div className="login-register-input">
@@ -102,8 +166,17 @@ const RegisterFormik = () => {
 
           <div className="login-register-input">
             <label htmlFor="account">Dollar Account No.</label>
-            <Field type="text" id="account" name="account" className="register-field" />
-            <ErrorMessage name="account" component="div" className="error-message" />
+            <Field
+              type="text"
+              id="account"
+              name="account"
+              className="register-field"
+            />
+            <ErrorMessage
+              name="account"
+              component="div"
+              className="error-message"
+            />
           </div>
 
           <button className="login-register-button" type="submit">
@@ -111,7 +184,8 @@ const RegisterFormik = () => {
             <i className="fas fa-chevron-right" />
           </button>
           <div>
-            <p className="navigation-link">Already have an account?{" "}
+            <p className="navigation-link">
+              Already have an account?{" "}
               <NavLink to={"/login"}>Login now!</NavLink>
             </p>
           </div>
