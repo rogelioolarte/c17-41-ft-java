8d41bf1ee4fe1a6deb211e4fb97b1cb3d8afc211
Rogelio Olarte <galaxino12@gmail.com>
2024-04-21 23:35:29 -0500

    Update HomePage.jsx

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import "../styles/styleHome.scss";
 import useProtectedRoute from "../hooks/useProtectedRoute";
 
 const HomePage = () => {
-  useProtectedRoute();
   return (
     <div className="home">
       <h1 className="home-title">Trade Bitcoin with PAYZO</h1>
